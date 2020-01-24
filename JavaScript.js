47c8d04a9643ad39787a9dd87fc3171e5bfc7300
amna1511 <amna.13aug@gmail.com>
2020-01-24 09:32:48 +0100

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+﻿function myFunction() {
+    var str1 = 'Hello this is my Orignal String';
+    var person = prompt("Please enter your String", "");
+    if (person == "Py" || person == "py") {
+        str1 = str1;
+    } else {
+
+        str1 = person.concat(", ").concat(str1);
+        
+    }
+
+    document.getElementById("demo").innerHTML = str1;
+  
+ 
+}
